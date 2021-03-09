eba84ec97ddd8f7a35416e4d9c7d528f3f0a647d
António Vieira <antoniofilipevieira@gmail.com>
2021-03-09 19:29:27 +0000

    Handle bottom navigation onNavigationItemSelected.

--- app/src/main/java/com/antoniovieira/dogsapp/ui/MainActivity.kt
+++ app/src/main/java/com/antoniovieira/dogsapp/ui/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.antoniovieira.dogsapp.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.antoniovieira.dogsapp.R
 import com.antoniovieira.dogsapp.databinding.ActivityMainBinding
 import com.antoniovieira.dogsapp.ui.home.HomeFragment
+import com.antoniovieira.dogsapp.ui.search.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var mCurrentFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +22,66 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(
-                R.id.fragmentContainer, HomeFragment.newInstance(), HomeFragment.TAG)
-                .commitNow()
+        setupUI()
 
+    }
+
+    private fun setupUI() {
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        binding.bottomNavigationView.selectedItemId = R.id.tab_home
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var selectedFragment: Fragment? = null
+        var selectedFragmentTag: String? = ""
+
+        when (item.itemId) {
+            R.id.tab_home -> {
+                selectedFragment = HomeFragment.newInstance()
+                selectedFragmentTag = HomeFragment.TAG
+            }
+
+            R.id.tab_search -> {
+                selectedFragment = SearchFragment.newInstance()
+                selectedFragmentTag = SearchFragment.TAG
+            }
+        }
+
+        var fragment = supportFragmentManager.findFragmentByTag(selectedFragmentTag)
+
+        mCurrentFragment?.takeIf { it.isVisible }?.let {
+            hideFragment(it)
+        }
+
+        if (fragment == null) {
+            fragment = selectedFragment
+        }
+
+        showFragment(fragment, selectedFragmentTag)
+
+        mCurrentFragment = fragment
+
+        return true
+    }
+
+    private fun showFragment(selectedFragment: Fragment?, selectedFragmentTag: String?) {
+        if (selectedFragment == null || selectedFragmentTag == null) return
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if (selectedFragment.isAdded) {
+            transaction.show(selectedFragment)
+        } else {
+            transaction.add(R.id.fragmentContainer, selectedFragment, selectedFragmentTag)
+        }
+
+        transaction.commitNow()
+    }
+
+    private fun hideFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.hide(fragment)
+        transaction.commitNow()
     }
 
 }
