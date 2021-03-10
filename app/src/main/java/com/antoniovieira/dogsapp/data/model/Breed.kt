138af04688a77c92c1f6bf935e9072351bb3a48b
António Vieira <antoniofilipevieira@gmail.com>
2021-03-10 22:33:01 +0000

    Added network response models.

---
+++ app/src/main/java/com/antoniovieira/dogsapp/data/model/Breed.kt
@@ -0,0 +1,13 @@
+package com.antoniovieira.dogsapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Breed(
+    val id: String,
+    val name: String,
+    val origin: String,
+    @SerializedName("breed_group")
+    val group: String,
+    val image: Image,
+    val temperament: String
+)
---
+++ app/src/main/java/com/antoniovieira/dogsapp/data/model/Image.kt
@@ -0,0 +1,6 @@
+package com.antoniovieira.dogsapp.data.model
+
+data class Image(
+    val id: String,
+    val url: String
+)
