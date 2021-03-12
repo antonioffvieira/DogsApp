25dd3051517e1c231c44ad2d14a1efbc7cf2e6e6
António Vieira <antoniofilipevieira@gmail.com>
2021-03-12 20:04:23 +0000

    Fixed missing change in previous commit.

---
+++ app/src/main/java/com/antoniovieira/dogsapp/ui/home/HomeModule.kt
@@ -0,0 +1,10 @@
+package com.antoniovieira.dogsapp.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.antoniovieira.dogsapp.di.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class HomeModule
