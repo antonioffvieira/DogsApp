645095996bb83420ca17fb3738dc755d9def336a
António Vieira <antoniofilipevieira@gmail.com>
2021-03-13 19:56:21 +0000

    Created function in HomeViewModel to search the breeds by name.

--- app/src/main/java/com/antoniovieira/dogsapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/antoniovieira/dogsapp/ui/search/SearchViewModel.kt
@@ -1,13 +1,44 @@
 package com.antoniovieira.dogsapp.ui.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.antoniovieira.dogsapp.data.BreedsRepository
+import com.antoniovieira.dogsapp.data.model.Breed
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(
     private val breedsRepository: BreedsRepository
 ) : ViewModel() {
 
+    private val compositeDisposables = CompositeDisposable()
 
+    private val _breeds = MutableLiveData<List<Breed>>()
+    val breeds: LiveData<List<Breed>> = _breeds
 
+    fun searchBreedByName(query: String) {
+        breedsRepository.searchBreedByName(query)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { _breeds.value = it },
+                onError = { handleError(it)},
+            )
+            .addTo(compositeDisposables)
+    }
+
+    private fun handleError(throwable: Throwable) {
+        // TODO
+        //  Handle error and show a popup
+    }
+
+    override fun onCleared() {
+        compositeDisposables.clear()
+        super.onCleared()
+    }
 }
