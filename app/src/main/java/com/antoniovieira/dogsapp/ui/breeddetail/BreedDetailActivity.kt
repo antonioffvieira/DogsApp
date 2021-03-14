4a4c182a894b38ea6f95700e837a5e51c345efdd
António Vieira <antoniofilipevieira@gmail.com>
2021-03-14 21:01:40 +0000

    Bind info to the UI in breed detail page.

--- app/src/main/java/com/antoniovieira/dogsapp/ui/breeddetail/BreedDetailActivity.kt
+++ app/src/main/java/com/antoniovieira/dogsapp/ui/breeddetail/BreedDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.antoniovieira.dogsapp.R
 import com.antoniovieira.dogsapp.data.model.Breed
 import com.antoniovieira.dogsapp.databinding.ActivityBreedDetailBinding
 
@@ -23,24 +24,34 @@ class BreedDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBreedDetailBinding
 
-    private var breed: Breed? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityBreedDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        breed = intent?.extras?.getParcelable(EXTRA_BREED) as? Breed
+        val breed: Breed = requireNotNull(intent?.extras?.getParcelable(EXTRA_BREED)) {
+            "The Bundle must have a breed"
+        }
 
         setupUI()
-
-        // TODO Bind data : Breed name, category, origin, temperament
+        setContent(breed)
     }
 
     private fun setupUI() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun setContent(breed: Breed) {
+        with(binding) {
+            toolbar.title = breed.name
+            breedNameValue.text = breed.name
+            // TODO Where do I get this value because the API doesn't have any category value in the request
+            breedCategoryValue.text = getString(R.string.empty_value)
+            breedOriginValue.text = breed.origin ?: getString(R.string.empty_value)
+            breedTemperamentValue.text = breed.temperament
         }
     }
 
